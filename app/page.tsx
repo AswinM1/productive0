ccfde6469f2fb0e6dfa1dd21a2d6356e1b1acf90
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-09-05 20:04:58 +0530

    start

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-  
+      <h1>The All in one Productivity Web</h1>
+      <p>Timers,Trackers,Leaderboards and Many more</p>
+      <button className="bg-black rounded-md px-2 py-2 cursor-pointer text-white">Get started</button>
     </div>
   );
 }
---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware() {},
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages:{
+        signIn:"/login"
+    }
+  }
+);
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/settings",
+    "/activity",
+    "/analytics",
+    "/leaderboard",
+    "/tokens",
+    "/user"
+
+  ],
+};
